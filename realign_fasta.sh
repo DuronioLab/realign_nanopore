d19a7fdded9822442f4a48629d5a3b65032ebebb
Markus Nevil <37672597+markus-nevil@users.noreply.github.com>
2022-12-06 11:05:44 -0500

    Update realign_fasta.sh

--- realign_fasta.sh
+++ realign_fasta.sh
@@ -64,7 +64,33 @@ done
 
 #Generate the consensus sequence
 module purge && module load medaka
-medaka_consensus -i ./plasmid_restart.fasta -d ${ref_fasta} -t 1 -o plasmid_restart_consensus
+medaka_consensus -i ./plasmid_restart.fasta -o plasmid_restart_consensus -d ${ref_fasta} -t 1
+
+## Add the beginning of the first 15 reads to the output, to check later
+
+# Set the number of times to repeat
+num_repeats=15
+
+# Read the input file into an array of lines
+mapfile -t lines < plasmid_restart.fasta
+
+echo "Here are the beginnings of the first "${num_repeats}" reads"
+# Iterate over the lines of the input file
+for line in "${lines[@]}"; do
+  # Check if the line starts with a ">" character
+  if [[ $line == ">"* ]]; then
+    # Print the first 40 characters of the next line
+    echo ${lines[$i+1]:0:40}
+    
+    # Decrement the number of repeats
+    num_repeats=$((num_repeats-1))
+  fi
+
+  # Stop repeating once the specified number of repeats has been reached
+  if [ $num_repeats -eq 0 ]; then
+    break
+  fi
+done
 
 echo "Removing Files..."
 
